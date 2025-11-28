90a1c34038a0ab135cf35a2b1505d35a1f867ecb
ColtGriff2k <coltgriff2k@yahoo.com>
2025-11-28 16:59:41 -0700

    Added HTML and JS
    Added webpage with JavaScript functions.

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+// var1
+var sent1 = "This is the beginning of the string ";
+// var2
+var sent2 = "and this is the end of the string.";
+// functions
+// popup of string 1 and 2
+window.alert(sent1 + sent2)
+//print of string 1 and 2
+document.write(sent1 + sent2)
