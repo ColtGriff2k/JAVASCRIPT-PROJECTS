01bee3c8eb2ca29163ebc821ca743edc9027b25f
ColtGriff2k <coltgriff2k@yahoo.com>
2025-12-02 14:19:44 -0700

    Added HTML with JS
    Added a webpage with JavaScript functions.

---
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+//BUTTON FUNCTION
+function my_first_function() {
+    var str = "This is the button text!";
+    document.getElementById("button_text").innerHTML = str;
+}
+// CONCATENTATE FUNCTION
+function my_second_function(){
+    var sentence = "The paragraph text ";
+    sentence += "will chance after clicking it!";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
